b946256ecf77a62108bf9ceb40c96d888e002b9e
Андрей <nevraev1109@mail.ru>
2023-01-21 21:53:08 +0500

    Добавьте файлы проекта.

---
+++ WpfApp5/MainWindow.xaml.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfApp5
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+        
+private void Add_Click(object sender, RoutedEventArgs e)
+        {
+            FrameworkElement sourceFrameworkElement = e.Source as FrameworkElement;
+            e.Handled = true;
+            _list.Text += Login.Text;
+        }
+    }
+}
